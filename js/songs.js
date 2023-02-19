5c00bdd778f3607ffe935e0690ca3fee15acc4d6
vitali007tut <vitali007@tut.by>
2023-02-19 22:51:22 +0300

    add player cust

---
+++ js/songs.js
@@ -0,0 +1,54 @@
+function CreatePlayer() {
+    var playListSongs = [
+      {
+        artist: 'Сваты',
+        title: 'Вот и стали дни короче',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Вот%20и%20стали%20дни%20короче.mp3',
+        duration: '2:17',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Всё повторится',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Всё%20повторится.mp3',
+        duration: '2:05',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Инструментал',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Инструментал.mp3',
+        duration: '3:25',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Календарь',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Календарь.mp3',
+        duration: '3:29',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Лето',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Лето.mp3',
+        duration: '1:25',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Погоня',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Погоня.mp3',
+        duration: '2:04',
+        howl: null
+      },
+      {
+        artist: 'Сваты',
+        title: 'Зарядка',
+        file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20%20Зарядка.mp3',
+        duration: '2:47',
+        howl: null
+      }
+    ]
+    return playListSongs;
+  }
